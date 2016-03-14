99532eb001935ce46177cdbf62135abedb5469a3
Siarhei Rasiukevich <s_rasiukevich@wargaming.net>
2016-03-14 18:34:14 +0300

    add ability to store specific value

--- lib/puppet/parser/functions/passgen.rb
+++ lib/puppet/parser/functions/passgen.rb
@@ -12,13 +12,19 @@ module Puppet::Parser::Functions
         expire = ChronicDuration.parse(args[1])
     end
 
+    if args[2]
+        gen_value = args[2]
+    else
+        gen_value = `pwgen -s -1 14`.chomp
+    end
+
     store = PStore.new(File.join(lookupvar('passgen::params::storage_path'), filename))
     pass = store.transaction { store['value'] }
     stored_expire = store.transaction { store['expire'] }
     expire_duration = store.transaction { store['expire_duration'] }
     if not pass or (expire and stored_expire ? Time.now.to_i > stored_expire : false) or expire_duration != args[1]
       pass = store.transaction do
-          store['value'] = `pwgen -s -1 14`.chomp
+          store['value'] = gen_value 
       end
       store.transaction do
           store['expire'] = expire ? Time.now.to_i + expire : expire
